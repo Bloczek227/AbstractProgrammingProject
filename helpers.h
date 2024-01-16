7101fb41c4e0c4c9a37938286f7e7b6017618563
Bloczek227 <74356705+Bloczek227@users.noreply.github.com>
2024-01-17 00:10:52 +0100

    Delete helpers.h

--- helpers.h
+++
@@ -1,8 +0,0 @@
-#ifndef PA_PROJ_HELPERS_H
-#define PA_PROJ_HELPERS_H
-
-constexpr double calculateReluFunction(double baseValue,int firstChangeLevel,int changeValue,int level){
-    return level<firstChangeLevel?baseValue:(level-firstChangeLevel+1)*changeValue;
-}
-
-#endif //PA_PROJ_HELPERS_H
